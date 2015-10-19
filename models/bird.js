acf3d4a351cd50bdfd2faa3185a0aae9182f6c31
Lauren Grant <dangerlauren@gmail.com>
2015-10-19 16:29:31 -0500

    added bird, user models

---
+++ models/bird.js
@@ -0,0 +1,15 @@
+// Dog model w/ Mongoose
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var birdSchema = new Schema({
+    name: { type: String, required: true },
+    latin: { type: String, required: true },
+    images: { type: String, required: true },
+    
+});
+
+var Bird = mongoose.model('Bird', birdSchema);
+
+// Make this available to our other files
+module.exports = Bird;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var Bird = require('../models/bird');
+var User = require('../models/user');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	
+	Bird.find({name: "Cardinal"}, function(err, bird) {
+		if(err) console.log(err);
+		console.log(bird);
+		res.render('index', { title: 'What the Duck?' });
+	});
+
+  
 });
+
+
 
 module.exports = router;
